2e08829385950734fc99e2781ccdcd0058e62788
atocchi <andrewtocchi@gmail.com>
2019-11-07 22:27:09 -0800

    --Testing Chips/Hooks

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Avatar from '@material-ui/core/Avatar';
+import Chip from '@material-ui/core/Chip';
+
+
 function App() {
+  const [off,setOff] = useState(false);
+  
+  const handleClick = () =>{
+  off ? setOff(false) : setOff(true);
+  }
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <Chip
+          avatar={<Avatar>D</Avatar>}
+          label="Dummy"
+          clickable
+          color="primary"
+          onClick={handleClick}
+          disabled={off}
+          />
         </p>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
