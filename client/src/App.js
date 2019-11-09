cef3d73325ef2a7537553bd496f0b65629ac8a52
atocchi <andrewtocchi@gmail.com>
2019-11-08 22:13:21 -0800

    --Added iterators for dummy data

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,30 @@ function App() {
   //using Hooks instead of classes to set state
   const [off,setOff] = useState(false);
   const [rend,setRend] = useState(true);
+  const [dum, setDum] = useState([{
+    title: 'Dummy',
+    bol1: true,
+    bol2: false,
+    bol3: false,
+    text: 'Lorem Ipsum arambum noculus ispum lorem aramabos amululant epsum ONE ',
+    img: 'https://i.imgur.com/Gc1NSVZ.png'
+  },{
+    title: 'Dummy2',
+    bol1: false,
+    bol2: true,
+    bol3: false,
+    text: 'Lorem Ipsum arambum noculus ispum lorem aramabos amululant epsum TWO',
+    img: 'https://i.imgur.com/Gc1NSVZ.png'
+  },
+  {
+    title: 'Dummy3',
+    bol1: false,
+    bol2: false,
+    bol3: true,
+    text: 'Lorem Ipsum arambum noculus ispum lorem aramabos amululant epsum THREE',
+    img: 'https://i.imgur.com/Gc1NSVZ.png'
+  }
+])
 
   const handleClick = () =>{
   off ? setOff(false) : setOff(true);
@@ -30,9 +54,11 @@ function App() {
           onClick={handleClick}
           disabled={off}
           />
-        
-        <Dummy rend={rend}/>
-        
+        {dum.map((item, key) =>
+        <Dummy 
+        key={item.title} title={item.title} text={item.text} img={item.img} bol={item.bol1} bol2={item.bol2} bol3={item.bol3}
+        />
+        )}
         
       
     </div>
--- client/src/components/dummy.js
+++ client/src/components/dummy.js
@@ -1,14 +1,32 @@
 import React, {useState} from 'react';
 
 function Dummy(props){
- if(props.rend === true){
+ if(props.bol === true){
     return(
         
         <div>
-            Hello World!
+            <h1>{props.title}</h1>
+            <img style={{width:'400px', height:'400px'}}src={props.img} />
+            <p>{props.text}</p>
         </div>
     )
- } else{
+ }else if (props.bol2 === true){
+    return(
+        
+        <div>
+            <h1>{props.title}</h1>
+            <img style={{width:'400px', height:'400px'}}src={props.img} />
+        </div>
+    )
+ }else if (props.bol3 === true){
+     return(
+        <div>
+            <h1>{props.title}</h1>
+            <img style={{width:'400px', height:'400px'}}src={props.img} />
+        </div>
+     )
+ }
+  else{
   return(
   <div>GoodBye World</div>
   )
