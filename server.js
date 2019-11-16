6d7934bcdc5231952e016404c5cf313939dad56a
Mike Brown <theforgesound@gmail.com>
2019-11-16 13:35:38 -0800

    state variable

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const mongoose = require("mongoose");
 const routes = require('./routes/index');
 const app = express();
 const PORT = process.env.PORT || 3001;
-
+const cors = require('cors');
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(cors());
 
 if(process.env.NODE_ENV === "production"){
     app.use(express.static("client/build"));
