44ad3460080cce601282a4d46c2f9e57a411247c
Zev Hoffman <zevubu@gmail.com>
2019-11-18 20:33:46 -0800

    added ammits navbar to page added needed and offered page.

--- client/src/pages/forms/neededForm.js
+++ client/src/pages/forms/neededForm.js
@@ -5,12 +5,36 @@ function neededForm (props){
     return(
         <div>
             <form>
+                {/* name */}
                 <label for="name">Name</label>
                 <input type="text" name="name" id="name-input"/>
-                
+                {/* email */}
                 <label for="email">email</label>
-                <email type="email" name="email" id="email-input"></email>
-                {/* name, email, linkToCFofV, familySize,numOfPet,petInfo,currentCity,willingToRelocate,housing*/}
+                <email type="email" name="email" id="email-input"/>
+                {/* linkToCFofV */}
+                <label for="link-to-pay">link to crowd fund or venmo.</label>
+                <email type="text" name="link-to-pay" id="link-to-pay-input"/>
+                {/* familySize */}
+                <label for="familySize">size of family</label>
+                <email type="email" name="email" id="email-input"/>
+
+                {/* 
+                familySize,
+                numOfPet,
+                petInfo,
+                currentCity,
+                willingToRelocate,
+                housing,
+                medicalSupplies, 
+                financeSupport,
+                food,
+                clothing,
+                houseHoldItems,
+                counsMed,
+                photoLinks
+                About
+                moreInfo
+                */}
             </form>
 
         </div>
