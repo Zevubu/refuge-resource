6d8a0df4a105938c62e3f8bdf682d486d46d9fb6
Mike Brown <theforgesound@gmail.com>
2019-12-18 21:44:09 -0800

    fixed image aspect ratios

--- client/src/components/dummyNeeded.js
+++ client/src/components/dummyNeeded.js
@@ -45,7 +45,7 @@ function DummyNeeded(props){
                 {props.cat ? <Chip label='Cats' icon={<PetsIcon />} /> : <Chip label='No Cats' icon={<NotInterestedIcon />}/>}  &nbsp;
             </h3> : ''}
 
-            <img style={{width:'65%', height:'65%', opacity:'1'}}src={props.img} alt={props.alt}/>
+            <img style={{height:'65%', opacity:'1'}}src={props.img} alt={props.alt}/>
             <p>{props.text}</p>
             <p>Contact: <Mailto email={props.contact}></Mailto></p>
             <hr />
